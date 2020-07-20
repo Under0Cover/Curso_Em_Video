eca7d4cb74d92c048352c58c45df1f9183648481
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-07-20 17:49:43 -0300

    Add files via upload

---
+++ JavaScript/Aula 13/Ambiente.js
@@ -0,0 +1,23 @@
+/* Laço Simples */
+/* Teste primeiro, Execução segundo */
+/* var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+} */
+
+/* Execução primeiro, Teste segundo */
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+/* Estrutura de repetição
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
