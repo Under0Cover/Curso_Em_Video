3fb99e9e8811122254c08b3936785bbf4a84a7f7
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-07-21 19:34:10 -0300

    Add files via upload

---
+++ JavaScript/Aula 14/Ambiente.js
@@ -0,0 +1,13 @@
+/* Usando While
+var contador = 1
+while (contador <= 5) {
+    console.log(`Número ${contador}`)
+    contador++
+}
+*/
+
+//-------- Usando o Laço For
+console.log('Vai começar:')
+for (contador = 1; contador <= 5; contador++) {
+    console.log(`Número ${contador}`)
+} console.log('FIM!!!')
