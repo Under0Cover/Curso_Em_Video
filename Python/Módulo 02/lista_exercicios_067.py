d2bfc60d93a6614dfcac3b1adeea99f4e5af434f
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-09-17 01:00:14 -0300

    Add files via upload

---
+++ Python/Módulo 02/lista_exercicios_067.py
@@ -0,0 +1,18 @@
+# DESAFIO 067
+# ESTRUTURA WHILE COM BREAK
+
+# FAÇA UM PROGRAMA QUE MOSTRE A TABUADA DE VÁRIOS NÚMEROS
+# O PROGRAMA SERÁ INTERROMPIDO QUANDO O NÚMERO FOR NEGATIVO
+
+
+while True:
+    numero = int(input('Digite um número para ser calculado na tabuada: '))
+    if numero < 0:
+        print('PROGRAMA TABUADA ENCERRADO!!!')
+        print('VOLTE SEMPRE')
+        print('-=-' * 20)
+        break
+    print('-=-' * 20)
+    for contador in range(1, 11):
+        print(f'{numero} X {contador:2} = {numero}')
+    print('-=-' * 20)
