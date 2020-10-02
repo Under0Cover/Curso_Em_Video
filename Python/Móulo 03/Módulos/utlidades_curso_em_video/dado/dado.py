2cba62436b2f431fa8077b50aa248c115cdc2599
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-10-02 01:50:29 -0300

    Add files via upload

--- Python/Móulo 03/Módulos/utlidades_curso_em_video/dado/dado.py
+++ Python/Móulo 03/Módulos/utlidades_curso_em_video/dado/dado.py
@@ -1,15 +1,18 @@
 # FUNÇÕES DE VALIDAÇÃO
 
 def leiaDinheiro(mensagem):
+    """
+    -> Função de validação de números
+    :param mensagem: Conferir a mensagem para ter certeza que é número
+    :return: Retorna um float
+    By Tales Oliver
+    """
     ok = False
-    valor = 0
     while True:
-        numero = str(input(mensagem))
-        if numero.isnumeric():
-            valor = int(numero)
-            ok = True
+        numero = str(input(mensagem)).replace(',', '.')
+        if numero.isalpha() or numero.strip() == '':
+            print('\033[0;31mERRO! Digite um número válido.')
+            print(f'{numero} não é um número válido\033[m')
         else:
-            print('\033[0;31mERRO! Digite um número válido.\033[m')
-        if ok:
-            break
-    return valor
+            ok = True
+            return float(numero)
